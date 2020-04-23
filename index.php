b5bca403b56a54b6142d9c0cac2ea184e190f3e5
youp bos <2107418@talnet.nl>
2020-04-23 14:16:20 +0200

    Uitwerking van de exercise

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+    $host = 'localhost:3306';
+    $db   = 'netland';
+    $user = 'root';
+    $pass = '';
+    $charset = 'utf8mb4';
+
+    $options = [
+        \PDO::ATTR_ERRMODE            => \PDO::ERRMODE_EXCEPTION,
+        \PDO::ATTR_DEFAULT_FETCH_MODE => \PDO::FETCH_ASSOC,
+        \PDO::ATTR_EMULATE_PREPARES   => false,
+    ];
+    $dsn = "mysql:host=$host;dbname=$db;charset=$charset";
+    try {
+        $pdo = new \PDO($dsn, $user, $pass, $options);
+    } catch (\PDOException $e) {
+        throw new \PDOException($e->getMessage(), (int)$e->getCode());
+    }
+
+    echo $pdo->getAttribute(PDO::ATTR_CONNECTION_STATUS);
